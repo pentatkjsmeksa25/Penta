a007c10b07f9b29f63e6ddc01b413af6ea6e7e25
28_PentaPutraT <penta.tkjsmeksa25@gmail.com>
2026-08-31 09:23:10 +0700

    Add files via upload

---
+++ GanjilGenap.py
@@ -0,0 +1,14 @@
+bilangan = int(input("Masukkan sebuah bilangan: "))
+
+if bilangan % 2 == 0:
+    print(bilangan, "adalah bilangan genap")
+else:
+    print(bilangan, "adalah bilangan ganjil")
+
+for i in range(100):
+    bilangan = int(input("Masukkan Sebuah Bilangan: "))
+
+    if bilangan % 2 == 0:
+        print(bilangan, "adalah bilangan genap")
+    else:
+        print(bilangan, "adalah bilangan ganjil")
