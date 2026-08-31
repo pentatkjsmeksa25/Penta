2eec4c7e82824f16c965ae2d6e75653ee695dd41
28_PentaPutraT <penta.tkjsmeksa25@gmail.com>
2026-08-31 10:16:33 +0700

    Add files via upload

---
+++ modul.py
@@ -0,0 +1,15 @@
+import fungsi
+print("Progam Ganjil Genap")
+fungsi.ganjil_genap()
+
+import fungsi
+print("Progam Perkalian")
+fungsi.perkalian()
+
+import fungsi
+print("Progam Keliling Persegi")
+fungsi.kelilingpersegi()
+
+import fungsi
+print("Progam Luas Persegi")
+fungsi.luaspersegi()
